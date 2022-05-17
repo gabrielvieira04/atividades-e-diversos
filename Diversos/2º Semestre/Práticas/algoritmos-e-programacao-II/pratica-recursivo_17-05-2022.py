1dc3ebe7ebf8b42058c414696e0077b5a5de843d
Brak3 <gabrielvieira040@outlook.com>
2022-05-17 19:43:42 -0300

    Update pratica-recursivo_17-05-2022.py
    Atualização do codigo, contendo a primeira solução

--- Diversos/2º Semestre/Práticas/algoritmos-e-programacao-II/pratica-recursivo_17-05-2022.py
+++ Diversos/2º Semestre/Práticas/algoritmos-e-programacao-II/pratica-recursivo_17-05-2022.py
@@ -1,1 +1,15 @@
 #Pratica realizada no dia 17-05-2022
+#Primeira solução, utilizando estrutura de repetição
+print("\x1b[2J\x1b[1;1H")
+n = int(input("Digite o número que deseja calcular o fatorial."))
+
+def n_fatorial(n):
+    print("\x1b[2J\x1b[1;1H")
+    fatorial = 1
+    for i in range(1,n+1):
+        fatorial = fatorial * i 
+    print(f"O fatorial de", n,  "é", fatorial)
+
+n_fatorial(n)
+
+#Segunda solução, utilizado recursividade.
